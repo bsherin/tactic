b1e79de6e542c6edace5c425d929758f53a8da88
bsherin <bsherin@northwestern.edu>
2015-08-19 13:41:37 -0500

    Check to see if connected to Mongo server successfully. If not print an error message and exit.

--- tactic_app/__init__.py
+++ tactic_app/__init__.py
@@ -4,22 +4,31 @@
 
 from flask import Flask
 import pymongo
-import sys
+import sys, os
 from pymongo import MongoClient
 from flask.ext.login import LoginManager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.socketio import SocketIO
 
-client = MongoClient()
-db = client.tacticdb
 
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
+try:
+    client = MongoClient("localhost", serverSelectionTimeoutMS=10)
+    client.server_info() # force connection on a request as the
+                         # connect=True parameter of MongoClient seems
+                         # to be useless here
+    db = client.tacticdb
 
-app = Flask(__name__)
-app.config.from_object('config')
+    login_manager = LoginManager()
+    login_manager.session_protection = 'strong'
+    login_manager.login_view = 'auth.login'
 
-login_manager.init_app(app)
-bootstrap = Bootstrap(app)
-socketio=SocketIO(app)
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+    login_manager.init_app(app)
+    bootstrap = Bootstrap(app)
+    socketio=SocketIO(app)
+
+except pymongo.errors.ServerSelectionTimeoutError as err:
+    print("There's a problem with the mongo server. Probably it's not running. ", err)
+    sys.exit()
