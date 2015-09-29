440f84758027478a5cfcc921d0cc3a35fbb10a56
bsherin <bsherin@northwestern.edu>
2015-09-29 14:04:42 -0500

    put up local runner

---
+++ tactic_app/tactic_run_no_gunicorn.py
@@ -0,0 +1,22 @@
+# The unused imports here are required so that the
+# various handlers are registered via decorators
+
+# Much of the setup is done in tactic_app/__init__.py
+# This avoids circular imports since the view functions make use
+# of things such as app, socketio, and db that are created in __init__.py
+import os
+
+print "entering tactic_run"
+
+from tactic_app import app
+print "imported app"
+from tactic_app import users
+from tactic_app.views import auth_views, main_views, user_manage_views
+from tactic_app import tiles, tokenizers
+from tactic_app import socketio
+
+# app.run(debug=True)
+
+print "about to execute socketio.run"
+socketio.run(app)
+# socketio.run(app, host='0.0.0.0') # Get iP address for this machine and add :5000
