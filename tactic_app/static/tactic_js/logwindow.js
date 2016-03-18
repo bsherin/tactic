838d44c85b510168b2c861ae2a75d6d08a178443
bsherin <bsherin@northwestern.edu>
2016-03-18 17:18:58 -0500

    Improvements to new log viewer window

--- tactic_app/static/tactic_js/logwindow.js
+++ tactic_app/static/tactic_js/logwindow.js
@@ -1,5 +1,14 @@
 
 function start_post_load() {
+    $("#console").sortable({
+            handle: '.panel-heading',
+            tolerance: 'pointer',
+            revert: 'invalid',
+            forceHelperSize: true
+        });
+}
 
 
+function closeLogItem(e) {
+    $(e.parentElement.parentElement).remove()
 }
