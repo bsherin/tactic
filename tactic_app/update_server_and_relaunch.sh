04213d9ebf033df072a0b287efa731347e34c1e7
bls910 <bsherin@northwestern.edu>
2022-10-27 16:48:36 -0500

    pushing a script
    Signed-off-by: bls910 <bsherin@northwestern.edu>

--- tactic_app/update_server_and_relaunch.sh
+++ tactic_app/update_server_and_relaunch.sh
@@ -1,4 +1,3 @@
-sudo cd /srv/tactic
-sudo git pull
+cd /srv/tactic && sudo git pull
 sudo docker pull bsherin/tactic -a
 sudo /srv/tactic/tactic_app/start_tactic.sh --root /srv/tactic --mdir /tacticdata/mongo/data
