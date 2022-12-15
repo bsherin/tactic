536b1246cb1b5e7752f922a3b5b3b44e521db6d8
brucesherin <bsherin@northwestern.edu>
2022-12-14 18:07:46 -0600

    Building images on server
    Signed-off-by: brucesherin <bsherin@northwestern.edu>

--- tactic_app/update_server_and_relaunch.sh
+++ tactic_app/update_server_and_relaunch.sh
@@ -1,3 +1,8 @@
 cd /srv/tactic && sudo git pull
-sudo docker pull bsherin/tactic -a
+# sudo docker pull bsherin/tactic -a
+sudo /usr/local/bin/docker-compose build tactic_base
+sudo /usr/local/bin/docker-compose build tactic_host
+sudo /usr/local/bin/docker-compose build tactic_main
+sudo /usr/local/bin/docker-compose build tactic_module_viewer
+sudo /usr/local/bin/docker-compose build tactic_tile
 sudo /srv/tactic/tactic_app/start_tactic.sh --root /srv/tactic --mdir /tacticdata/mongo/data
