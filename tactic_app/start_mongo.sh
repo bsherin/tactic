3b68c4f0e9f2e0a0d45d7a6a204af9954ce05d72
bsherin <bsherin@northwestern.edu>
2023-11-15 19:57:23 -0600

    Working on mongo_watcher. Some repository improvements
    Signed-off-by: bsherin <bsherin@northwestern.edu>

---
+++ tactic_app/start_mongo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mongo_dir="mongo-volume"
+mongo_uri="tactic-mongo"
+
+sudo docker run -d \
+  -v $mongo_dir:/data/db \
+  -p 27017:27017 \
+  --name $mongo_uri \
+  --restart always \
+  --network=tactic-net \
+  mongo:latest --replSet rs0
---
+++ tactic_app/start_mongo_watcher.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+use_remote_db="False"
+use_remote_repo="False"
+use_remote_repo="False"
+use_remote_repo_key="False"
+remote_username=
+remote_password=
+remote_key_file="None"
+mongo_uri="tactic-mongo"
+restart_policy="on-failure:5"
+arm_string="-arm64"
+
+sudo docker run -d \
+  --name mongo_watcher \
+  --restart $restart_policy \
+  --label my_id=pool_watcher \
+  --label owner=host \
+  --label parent=host \
+  --label other_name=pool_watcher \
+  --network=tactic-net \
+  --init \
+  -e MONGO_URI=$mongo_uri \
+	-e USE_REMOTE_DATABASE=$use_remote_db \
+  -e USE_REMOTE_REPOSITORY=$use_remote_repo \
+  -e USE_REMOTE_REPOSITORY_KEY=$use_remote_repo_key \
+  -e REMOTE_USERNAME=$remote_username \
+  -e REMOTE_PASSWORD=$remote_password \
+  -e REMOTE_KEY_FILE=$remote_key_file \
+  bsherin/tactic:mongo-watcher$arm_string
+
