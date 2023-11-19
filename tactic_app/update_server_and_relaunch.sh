27d7948ed47f70b2b1af430a92084b4f44614f04
bsherin <bsherin@northwestern.edu>
2023-11-19 11:12:34 -0600

    Working on mongo_watcher. Some repository improvements
    Signed-off-by: bsherin <bsherin@northwestern.edu>

--- tactic_app/update_server_and_relaunch.sh
+++ tactic_app/update_server_and_relaunch.sh
@@ -8,4 +8,5 @@ sudo /usr/local/bin/docker-compose build tactic_main
 sudo /usr/local/bin/docker-compose build tactic_module_viewer
 sudo /usr/local/bin/docker-compose build tactic_tile
 sudo /usr/local/bin/docker-compose build tactic_pool_watcher
+sudo /user/local/bin/docker-compose build mongo_watcher
 sudo /srv/tactic/tactic_app/start_tactic.sh --root /srv/tactic --mdir /tacticdata4/mongo/data --pdir /tacticdata4/pool
