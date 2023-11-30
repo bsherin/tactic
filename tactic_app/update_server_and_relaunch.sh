42c98f3bceb465ba9c04c86dbcb8a1aeaf2ac9aa
bsherin <bsherin@northwestern.edu>
2023-11-30 17:40:50 -0600

    fixes to up_tactic.sh and update_server_and_relaunch.sh
    Signed-off-by: bsherin <bsherin@northwestern.edu>

--- tactic_app/update_server_and_relaunch.sh
+++ tactic_app/update_server_and_relaunch.sh
@@ -2,5 +2,5 @@
 cd /srv/tactic && sudo git pull
 # sudo docker pull bsherin/tactic -a
 cd /srv/tactic/tactic_app
-sudo /usr/local/bin/docker compose --env-file server.env --profile build_project build
+sudo /usr/bin/docker compose --env-file server.env --profile build_project build
 sudo /srv/tactic/tactic_app/up_tactic.sh --env-file server.env
