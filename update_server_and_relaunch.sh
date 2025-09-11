ff7c68776354f973de3e96253022f73b32564b34
bsherin <bsherin@northwestern.edu>
2025-09-11 10:46:20 -0500

    push out new update_server_and_relaunch.sh
    Signed-off-by: bsherin <bsherin@northwestern.edu>

--- update_server_and_relaunch.sh
+++ update_server_and_relaunch.sh
@@ -2,5 +2,6 @@
 cd /srv/tactic && sudo git pull
 # sudo docker pull bsherin/tactic -a
 cd /srv/tactic
-sudo /usr/bin/docker compose --env-file server.env --profile build_project build
+sudo /user/bin/docker compose --env-file server.env build tactic_base
+sudo /usr/bin/docker compose --env-file server.env --profile build_project build --no-parallel
 sudo /srv/tactic/up_tactic.sh --env-file server.env
