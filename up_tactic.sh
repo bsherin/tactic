ec7b7fbc73b42ac311224544f73bbb3319fb6a18
bsherin <bsherin@northwestern.edu>
2025-10-08 12:55:12 -0500

    removing sudo from up_tactic.sh
    Signed-off-by: bsherin <bsherin@northwestern.edu>

--- up_tactic.sh
+++ up_tactic.sh
@@ -25,15 +25,15 @@ done
 
 if [ $up_only == "False" ] ; then
   echo "*** removing tactic containers ***"
-  num=$(sudo docker ps --filter label="project=tactic" -aq | wc -l)
+  num=$(docker ps --filter label="project=tactic" -aq | wc -l)
   if [ $num != "0" ] ; then
-    sudo docker ps --filter label="project=tactic" -aq | xargs sudo docker stop | xargs sudo docker rm
+    docker ps --filter label="project=tactic" -aq | xargs sudo docker stop | xargs sudo docker rm
   fi
   echo "*** removing aux containers ***"
-  num=$(sudo docker ps --filter label="project=tactic_aux" -aq | wc -l)
+  num=$(docker ps --filter label="project=tactic_aux" -aq | wc -l)
   if [ $num != "0" ] ; then
-    sudo docker ps --filter label="project=tactic_aux" -aq | xargs sudo docker stop | xargs sudo docker rm
+    docker ps --filter label="project=tactic_aux" -aq | xargs sudo docker stop | xargs sudo docker rm
   fi
 fi
 
-sudo docker compose --env-file $env_file --profile $profile up --detach
+ocker compose --env-file $env_file --profile $profile up --detach
