6fe7b7169f3c888000b47c484249490349b3c1e8
bsherin <bsherin@northwestern.edu>
2025-10-29 16:22:10 -0500

    added region back to environments

--- shared_python/aws_helpers.py
+++ shared_python/aws_helpers.py
@@ -5,7 +5,7 @@ def get_sms_parameter(name, default=None):
     Fetches a parameter from AWS Systems Manager Parameter Store.
     Returns the parameter value or default if not found.
     """
-    ssm = boto3.client('ssm')  # Adjust region as needed
+    ssm = boto3.client('ssm', region="us-east-2")  # Adjust region as needed
     try:
         response = ssm.get_parameter(Name=name, WithDecryption=True)
         return response['Parameter']['Value']
