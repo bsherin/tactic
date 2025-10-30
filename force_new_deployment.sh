c04591e23b746111c0440f5b638a962dcda75327
bsherin <bsherin@northwestern.edu>
2025-10-30 13:15:15 -0500

    some useful scripts

---
+++ force_new_deployment.sh
@@ -0,0 +1,9 @@
+export AWS_REGION=us-east-2
+export SERVICE=$1
+export CLUSTER=tactic-cluster
+
+aws ecs update-service \
+  --region "${AWS_REGION}" \
+  --cluster "${TACTIC_CLUSTER}" \
+  --service "${SERVICE}" \
+  --force-new-deployment
--- push_images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker push -q bsherin/tactic:base
-docker push -q bsherin/tactic:host
-docker push -q bsherin/tactic:main
-docker push -q bsherin/tactic:module_viewer
-docker push -q bsherin/tactic:tile
