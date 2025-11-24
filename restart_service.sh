bd84bd02d557dee2b7d3727832ec3406d717a90d
bsherin <bsherin@northwestern.edu>
2025-11-24 13:17:13 -0600

    A bunch of fixes to deal with the fact that the number of queues was exploding.

--- restart_service.sh
+++ restart_service.sh
@@ -1,13 +1,23 @@
-
 export SERVICE=$1
 
-TASKS=( $(aws ecs list-tasks --cluster tactic-cluster --service-name ${SERVICE}--query 'taskArns' --output text) )
+# List the tasks correctly (space fixed before --query)
+TASKS=( $(aws ecs list-tasks \
+    --cluster tactic-cluster \
+    --service-name "${SERVICE}" \
+    --query 'taskArns' \
+    --output text) )
+
+# Stop tasks
 for ARN in "${TASKS[@]}"; do
-  aws ecs stop-task --cluster tactic-cluster --task "$ARN" --reason "forced reset"
+  aws ecs stop-task \
+    --cluster tactic-cluster \
+    --task "$ARN" \
+    --reason "forced reset"
 done
 
+# Force new deployment
 aws ecs update-service \
   --region us-east-2 \
   --cluster tactic-cluster \
-  --service ${SERVICE} \
+  --service "${SERVICE}" \
   --force-new-deployment
