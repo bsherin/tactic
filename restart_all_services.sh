d75a596ae55e698a2dbcb16be3653b2007894619
bsherin <bsherin@northwestern.edu>
2025-11-24 13:26:27 -0600

    script fix

--- restart_all_services.sh
+++ restart_all_services.sh
@@ -1,4 +1,4 @@
-for sname in "tactic-main-service" "tactic-module-viewer" "tactic-pool-watcher-s3" "tactic-tile"
+for sname in "tactic-main-service" "tactic-module-viewer" "tactic-pool-watcher-s3" "tactic-tile-pool"
   do
     ./restart_service.sh $sname
   done
